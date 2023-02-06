bb12f88fe724ce09617448febfe4e460fd0e690f
laxsamy <laxsamy@gmail.com>
2023-02-06 15:05:44 +0000

    first code 1234

---
+++ src/main/java/uk/axone/devintest2/Applications2.java
@@ -0,0 +1,7 @@
+package uk.axone.devintest2;
+
+public class Applications2 {
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
---
+++ src/test/java/ApplicationDemo.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class ApplicationDemo {
+    public class Test_Application {
+        @Test
+        public void test1() {
+            System.out.println("running test1");
+
+        }
+    }
